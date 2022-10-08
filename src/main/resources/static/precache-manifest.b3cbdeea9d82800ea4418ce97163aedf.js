9e9a8aacb38949801b83cbfc9e5f3bb3d58ee781
黄克凡 <w545459363@126.com>
2022-10-08 16:58:29 +0800

    add log 1.4.4 fix

---
+++ src/main/resources/static/precache-manifest.b3cbdeea9d82800ea4418ce97163aedf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052c6ef046d404ca2532c4865c059814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d1e0b930d77d50d9400",
+    "url": "/static/css/2.5f638d4f.chunk.css"
+  },
+  {
+    "revision": "c8859e2ed640ee26553c",
+    "url": "/static/css/main.56aa1d77.chunk.css"
+  },
+  {
+    "revision": "0d1e0b930d77d50d9400",
+    "url": "/static/js/2.736bc460.chunk.js"
+  },
+  {
+    "revision": "c8859e2ed640ee26553c",
+    "url": "/static/js/main.be9ffa87.chunk.js"
+  },
+  {
+    "revision": "8a0a964fef2ade9579a0",
+    "url": "/static/js/runtime-main.ab73b915.js"
+  }
+]);
