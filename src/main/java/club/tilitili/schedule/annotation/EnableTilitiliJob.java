96f84449ca959b90b334c45fb0d0d76fb2d4f849
z-ero <w545459363@126.com>
2023-09-10 04:34:25 +0800

    update

--- src/main/java/club/tilitili/schedule/annotation/EnableTilitiliJob.java
+++ src/main/java/club/tilitili/schedule/annotation/EnableTilitiliJob.java
@@ -2,14 +2,12 @@ package club.tilitili.schedule.annotation;
 
 import club.tilitili.schedule.TilitiliScheduleAutoConfiguration;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@EnableAsync
 @Import(TilitiliScheduleAutoConfiguration.class)
 public @interface EnableTilitiliJob {
 }
